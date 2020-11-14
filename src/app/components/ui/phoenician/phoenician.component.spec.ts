0c9888375f5797b269412dc41bf4320d700abd87
erellsworth <edward.ellsworth@gmail.com>
2020-11-14 11:22:12 -0500

    Angular update

--- src/app/components/ui/phoenician/phoenician.component.spec.ts
+++ src/app/components/ui/phoenician/phoenician.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PhoenicianComponent } from './phoenician.component';
 
@@ -6,7 +6,7 @@ describe('PhoenicianComponent', () => {
   let component: PhoenicianComponent;
   let fixture: ComponentFixture<PhoenicianComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PhoenicianComponent ]
     })
