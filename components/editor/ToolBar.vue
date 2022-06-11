5b4661dac93e4044025a49e3408affb5ac4bbe31
erellsworth <edward.ellsworth@gmail.com>
2022-06-11 09:49:54 -0400

    Improve toolbar UI

--- components/editor/ToolBar.vue
+++ components/editor/ToolBar.vue
@@ -3,108 +3,119 @@
     <b-button
       @click="editor.chain().focus().toggleBold().run()"
       :class="{ 'is-info': editor.isActive('bold') }"
-    >
-      bold
-    </b-button>
+      icon-right="format-bold"
+      title="Bold"
+    />
     <b-button
       @click="editor.chain().focus().toggleItalic().run()"
       :class="{ 'is-info': editor.isActive('italic') }"
-    >
-      italic
-    </b-button>
+      icon-right="format-italic"
+      title="italic"
+    />
     <b-button
       @click="editor.chain().focus().toggleStrike().run()"
       :class="{ 'is-info': editor.isActive('strike') }"
-    >
-      strike
-    </b-button>
+      icon-right="format-strikethrough"
+      title="strikethrough"
+    />
+
     <b-button
       @click="editor.chain().focus().toggleCode().run()"
       :class="{ 'is-info': editor.isActive('code') }"
-    >
-      code
-    </b-button>
-    <b-button @click="editor.chain().focus().unsetAllMarks().run()">
-      clear marks
-    </b-button>
-    <b-button @click="editor.chain().focus().clearNodes().run()">
-      clear nodes
-    </b-button>
+      icon-right="code-tags"
+      title="Inline code"
+    />
+
+    <b-button
+      @click="clearFormat"
+      icon-right="format-clear"
+      title="remove inline formatting"
+    />
+
     <b-button
       @click="editor.chain().focus().setParagraph().run()"
-      :class="{ 'is-info': editor.isActive('paragraph') }"
-    >
-      paragraph
-    </b-button>
+      :class="{
+        'is-info': editor.isActive('paragraph'),
+      }"
+      label="P"
+    />
+
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      @click="toggleHeading(1)"
       :class="{ 'is-info': editor.isActive('heading', { level: 1 }) }"
-    >
-      h1
-    </b-button>
+      label="H1"
+    />
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      @click="toggleHeading(2)"
       :class="{ 'is-info': editor.isActive('heading', { level: 2 }) }"
-    >
-      h2
-    </b-button>
+      label="H2"
+    />
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      @click="toggleHeading(3)"
       :class="{ 'is-info': editor.isActive('heading', { level: 3 }) }"
-    >
-      h3
-    </b-button>
+      label="H3"
+    />
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+      @click="toggleHeading(4)"
       :class="{ 'is-info': editor.isActive('heading', { level: 4 }) }"
-    >
-      h4
-    </b-button>
+      label="H4"
+    />
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+      @click="toggleHeading(5)"
       :class="{ 'is-info': editor.isActive('heading', { level: 5 }) }"
-    >
-      h5
-    </b-button>
+      label="H5"
+    />
     <b-button
-      @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+      @click="toggleHeading(6)"
       :class="{ 'is-info': editor.isActive('heading', { level: 6 }) }"
-    >
-      h6
-    </b-button>
+      label="H6"
+    />
     <b-button
       @click="editor.chain().focus().toggleBulletList().run()"
       :class="{ 'is-info': editor.isActive('bulletList') }"
-    >
-      bullet list
-    </b-button>
+      icon-right="format-list-bulleted"
+      title="Bullet list"
+    />
     <b-button
       @click="editor.chain().focus().toggleOrderedList().run()"
       :class="{ 'is-info': editor.isActive('orderedList') }"
-    >
-      ordered list
-    </b-button>
+      icon-right="format-list-numbered"
+      title="Ordered list"
+    />
     <b-button
       @click="editor.chain().focus().toggleCodeBlock().run()"
       :class="{ 'is-info': editor.isActive('codeBlock') }"
-    >
-      code block
-    </b-button>
+      icon-right="code-not-equal-variant"
+      title="code block"
+    />
     <b-button
       @click="editor.chain().focus().toggleBlockquote().run()"
       :class="{ 'is-info': editor.isActive('blockquote') }"
-    >
-      blockquote
-    </b-button>
-    <b-button @click="editor.chain().focus().setHorizontalRule().run()">
-      horizontal rule
-    </b-button>
-    <b-button @click="editor.chain().focus().setHardBreak().run()">
-      hard break
-    </b-button>
-    <b-button @click="editor.chain().focus().undo().run()">undo</b-button>
-    <b-button @click="editor.chain().focus().redo().run()">redo</b-button>
-    <b-button @click="buttonClicked('logOutput')">log output</b-button>
+      icon-right="format-quote-close"
+      title="Blockquote"
+    />
+
+    <b-button
+      @click="editor.chain().focus().setHorizontalRule().run()"
+      title="horizontal rule"
+      label="---"
+    />
+
+    <b-button
+      @click="editor.chain().focus().undo().run()"
+      title="Undo"
+      icon-right="undo"
+    />
+    <b-button
+      @click="editor.chain().focus().redo().run()"
+      title="Redo"
+      icon-right="redo"
+    />
+    <b-button
+      @click="buttonClicked('logOutput')"
+      title="Show output in console"
+      icon-right="console"
+    />
   </div>
 </template>
 
@@ -116,6 +127,14 @@ export default {
     buttonClicked(eventName) {
       this.$emit(eventName);
     },
+    clearFormat() {
+      this.editor.chain().focus().unsetAllMarks().run();
+      this.editor.chain().focus().clearNodes().run();
+    },
+    toggleHeading(level) {
+      this.editor.chain().focus().toggleHeading({ level }).run();
+    },
+    launchMediaLibrary() {},
   },
 };
 </script>
