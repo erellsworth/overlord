788d7862746276fcd498520c3bef27c38a3bfbf9
erellsworth <edward.ellsworth@gmail.com>
2024-11-23 10:38:33 -0500

    Remove request library

--- src/app/dashboard/media-library/image-editor/cropper/cropper.component.ts
+++ src/app/dashboard/media-library/image-editor/cropper/cropper.component.ts
@@ -1,5 +1,16 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { CropperPosition, ImageCroppedEvent, ImageCropperComponent } from 'ngx-image-cropper';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  CropperPosition,
+  ImageCroppedEvent,
+  ImageCropperComponent,
+} from 'ngx-image-cropper';
 import { CropEvent, Image } from '../../../../../../interfaces/media';
 import { CardModule } from 'primeng/card';
 import { ImageModule } from 'primeng/image';
@@ -8,38 +19,33 @@ import { delay } from 'rxjs';
 @Component({
   selector: 'app-cropper',
   standalone: true,
-  imports: [
-    CardModule,
-    ImageCropperComponent,
-    ImageModule
-  ],
+  imports: [CardModule, ImageCropperComponent, ImageModule],
   templateUrl: './cropper.component.html',
-  styleUrl: './cropper.component.scss'
+  styleUrl: './cropper.component.scss',
 })
 export class CropperComponent {
-
   @Input({ required: true }) image!: Image;
   @Output() imageCropped = new EventEmitter<CropEvent>();
 
   public get src(): string {
-    if (!this.image) { return '' }
-    return Boolean(this.image.data.id) ? `api/getImageFromUrl?url=${this.image.full}` : this.image.full;
+    if (!this.image) {
+      return '';
+    }
+    return this.image.full; //Boolean(this.image.data.id) ? `api/getImageFromUrl?url=${this.image.full}` : this.image.full;
   }
 
   public cropped(event: ImageCroppedEvent, size: CropEvent['size']): void {
     const { height, width } = event;
     const left = Math.round(event.cropperPosition.x1);
     const top = Math.round(event.cropperPosition.y1);
-    this.imageCropped.emit(
-      {
-        size,
-        crop: {
-          left,
-          top,
-          width,
-          height
-        }
-      })
+    this.imageCropped.emit({
+      size,
+      crop: {
+        left,
+        top,
+        width,
+        height,
+      },
+    });
   }
-
 }
