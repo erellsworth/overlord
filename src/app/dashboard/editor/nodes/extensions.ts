9c98843dac4c9ac3da1a897d6ae3ed934cbf878b
Edward <edward@Mac.fios-router.home>
2025-02-09 13:48:16 -0500

    Add columns extension

--- src/app/dashboard/editor/nodes/extensions.ts
+++ src/app/dashboard/editor/nodes/extensions.ts
@@ -1,23 +1,25 @@
-import { Injector } from "@angular/core";
+import { Injector } from '@angular/core';
 import { Extension, Mark, Node } from '@tiptap/core';
-import ImageFigure from "./ImageFigure";
-import FigCaption from "./FigCaption";
-import VideoNode from "./VideoNode";
-import StarterKit from "@tiptap/starter-kit";
-import Image from "@tiptap/extension-image";
-import Link from "@tiptap/extension-link";
+import ImageFigure from './ImageFigure';
+import FigCaption from './FigCaption';
+import VideoNode from './VideoNode';
+import StarterKit from '@tiptap/starter-kit';
+import Image from '@tiptap/extension-image';
+import Link from '@tiptap/extension-link';
+import ColumnExtension from './columns';
 
 type ExtensionTypes = Node | Mark | Extension;
 
 const Extensions = (injector: Injector): ExtensionTypes[] => {
-    return [
-        StarterKit,
-        Image,
-        Link,
-        FigCaption(injector),
-        ImageFigure(injector),
-        VideoNode(injector),
-    ]
-}
+  return [
+    ColumnExtension,
+    StarterKit,
+    Image,
+    Link,
+    FigCaption(injector),
+    ImageFigure(injector),
+    VideoNode(injector),
+  ];
+};
 
 export default Extensions;
